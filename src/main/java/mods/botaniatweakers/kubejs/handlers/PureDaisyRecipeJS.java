32bf6e19d19d9a38dbb418ce173219778d8e8ce0
Yoghurt4C <yoghurt4242@gmail.com>
2020-12-23 12:25:15 +0300

    Well, it's doing something.

--- src/main/java/mods/botaniatweakers/kubejs/handlers/PureDaisyRecipeJS.java
+++ src/main/java/mods/botaniatweakers/kubejs/handlers/PureDaisyRecipeJS.java
@@ -1,76 +1,75 @@
 package mods.botaniatweakers.kubejs.handlers;
 
-import com.google.common.collect.ImmutableSet;
-import dev.latvian.kubejs.item.ingredient.IngredientJS;
 import dev.latvian.kubejs.recipe.RecipeJS;
 import dev.latvian.kubejs.util.ListJS;
 import mods.botaniatweakers.BotaniaTweakers;
 import mods.botaniatweakers.util.TweakerUtil;
-import net.minecraft.block.Block;
 import net.minecraft.block.BlockState;
+import net.minecraft.util.Identifier;
 import net.minecraft.util.JsonHelper;
+import net.minecraft.util.registry.Registry;
 import vazkii.botania.api.recipe.StateIngredient;
 import vazkii.botania.common.crafting.StateIngredientHelper;
 
 public class PureDaisyRecipeJS extends RecipeJS {
+    int index = 0;
     int time = 150;
+    StateIngredient ing;
+    BlockState out;
 
     @Override
     public void create(ListJS args) {
-        int i = 0;
-        if (args.size() > 3) {
-            this.id = TweakerUtil.toId(args.get(i));
-            i++;
-        }
+        int i = 1;
 
-        this.outputItems.add(TweakerUtil.toItemStackJS(args.get(i)));
+        this.out = TweakerUtil.toState(TweakerUtil.toString(args.get(i)));
         i++;
 
-        this.inputItems.add(TweakerUtil.toItemStackJS(args.get(i)));
+        this.ing = TweakerUtil.toStateIngredient(TweakerUtil.toString(args.get(i)));
         i++;
 
         this.time = TweakerUtil.toInt(args.get(i));
+
+        if (args.size() > 3) {
+            this.id = TweakerUtil.toId(args.get(0));
+        } else {
+            Identifier block = Registry.BLOCK.getId(out.getBlock());
+            this.id = TweakerUtil.toId("pure_daisy/" + block.getPath() + "_" + index);
+        }
     }
 
     @Override
     public void deserialize() {
-        if (this.json.get("id") != null) {
-            this.id = BotaniaTweakers.getId(this.json.get("id").getAsString());
-        }
-
         if (json.get("output").isJsonObject()) {
-            this.outputItems.add(IngredientJS.of(TweakerUtil.toState(json, "output").getBlock()).getFirst());
-        } else {
-            this.outputItems.add(IngredientJS.of(json.get("output")).getFirst());
+            this.out = TweakerUtil.toState(json, "output");
         }
 
         if (json.get("input").isJsonObject()) {
-            StateIngredient in = StateIngredientHelper.deserialize(JsonHelper.getObject(json, "input"));
-            for (BlockState state : in.getDisplayed()) {
-                this.inputItems.add(IngredientJS.of(state.getBlock()));
-            }
-        } else {
-            this.inputItems.add(IngredientJS.of(json.get("input")));
+            this.ing = StateIngredientHelper.deserialize(JsonHelper.getObject(json, "input"));
         }
+
         this.time = JsonHelper.getInt(json, "time", this.time);
+
+        if (this.json.get("id") != null) {
+            this.id = BotaniaTweakers.getId(this.json.get("id").getAsString());
+        } else {
+            Identifier block = Registry.BLOCK.getId(out.getBlock());
+            this.id = TweakerUtil.toId("pure_daisy/" + block.getPath() + "_" + index);
+        }
+
     }
 
     @Override
     public void serialize() {
         this.json.addProperty("type", this.type.toString());
 
-        BlockState out = Block.getBlockFromItem(this.outputItems.get(0).getItem()).getDefaultState();
         this.json.add("output", StateIngredientHelper.serializeBlockState(out));
 
-        ImmutableSet.Builder<Block> blocks = ImmutableSet.builder();
-        for (IngredientJS in : this.inputItems) {
-            in.getStacks().forEach(stack -> blocks.add(Block.getBlockFromItem(stack.getItem())));
-        }
-        StateIngredient in = StateIngredientHelper.of(blocks.build());
-        this.json.add("input", in.serialize());
+        this.json.add("input", ing.serialize());
 
         if (this.time >= 0 && this.time != 150) {
             this.json.addProperty("time", this.time);
         }
+
+        index++;
     }
 }
--- src/main/resources/data/botaniatweakers/kubejs/tweaker_test.js
+++ src/main/resources/data/botaniatweakers/kubejs/tweaker_test.js
@@ -3,72 +3,72 @@
 var shouldLoad = false
 
 if (shouldLoad) {
-settings.logAddedRecipes = true
-settings.logRemovedRecipes = true
-settings.logSkippedRecipes = false
-settings.logErroringRecipes = true
+    settings.logAddedRecipes = true
+    settings.logRemovedRecipes = true
+    settings.logSkippedRecipes = false
+    settings.logErroringRecipes = true
 
-events.listen('recipes', event => {
+    events.listen('recipes', event => {
 
-    event.recipes.botania.brew({
-        id: 'rot_test',
-        brew: 'botania:absorption',
-        ingredients: [
-            'minecraft:apple', 'minecraft:iron_ingot', 'minecraft:rotten_flesh'
-        ]
+        event.recipes.botania.brew({
+            id: 'rot_test',
+            brew: 'botania:absorption',
+            ingredients: [
+                'minecraft:apple', 'minecraft:iron_ingot', 'minecraft:rotten_flesh'
+            ]
+        })
+        event.recipes.botania.brew('grot_test', 'botania:absorption', ['minecraft:golden_apple * 5', 'minecraft:iron_ingot * 2 {tag:1b}', 'minecraft:rotten_flesh'])
+
+        event.recipes.botania.elven_trade({
+            id: 'scrot_test',
+            output: [
+                'minecraft:apple'
+            ],
+            ingredients: [
+                'minecraft:carrot', 'minecraft:carrot'
+            ]
+        })
+        event.recipes.botania.elven_trade('scrunt_test', ['botania:tiny_potato * 4 {display: {Name: \'{"text":"pahimar"}\'}}'], ['minecraft:potato', 'minecraft:potato'])
+
+        event.recipes.botania.mana_infusion({
+            id: 'coom',
+            output: 'minecraft:gold_block',
+            input: 'minecraft:brick',
+            mana: 500,
+            catalyst: 'minecraft:bricks'
+        })
+        event.recipes.botania.mana_infusion('coom2', 'minecraft:diamond_block', 'minecraft:brick', 1000, 'minecraft:gold_block', 'botania:flower_cycle')
+        event.recipes.botania.mana_infusion('tag_test', '#minecraft:leaves', '#minecraft:coals', 1000, '#minecraft:logs')
+
+        event.recipes.botania.petal_apothecary({
+            id: 'vroom',
+            output: 'minecraft:clay_ball',
+            ingredients: ['minecraft:iron_ingot', 'minecraft:gold_ingot']
+        })
+        event.recipes.botania.petal_apothecary('gloom', 'minecraft:diamond', ['minecraft:clay_ball', 'minecraft:clay_ball'])
+
+        event.recipes.botania.pure_daisy({
+            id: 'woomy',
+            output: 'minecraft:coarse_dirt',
+            input: 'minecraft:coal_ore',
+            time: 20
+        })
+        event.recipes.botania.pure_daisy('yoom', 'minecraft:gold_ore', 'minecraft:bricks', 80)
+
+        event.recipes.botania.runic_altar({
+            id: 'roony',
+            output: 'minecraft:clay_ball',
+            ingredients: ['minecraft:feather', 'minecraft:feather'],
+            mana: 3000
+        })
+        event.recipes.botania.runic_altar('loomy', 'minecraft:feather', ['minecraft:clay_ball', 'minecraft:clay_ball'], 500)
+
+        event.recipes.botania.terra_plate({
+            id: 'all_the_mana_is_gone',
+            result: 'minecraft:clay_ball',
+            ingredients: ['minecraft:diamond', 'botania:tiny_potato'],
+            mana: 1500000
+        })
+        event.recipes.botania.terra_plate('awful', 'minecraft:golden_apple*10', ['minecraft:potato', 'minecraft:golden_carrot'], 1500)
     })
-    event.recipes.botania.brew('grot_test', 'botania:absorption', ['minecraft:golden_apple * 5', 'minecraft:iron_ingot * 2 {tag:1b}', 'minecraft:rotten_flesh'])
-
-    event.recipes.botania.elven_trade({
-        id: 'scrot_test',
-        output: [
-            'minecraft:apple'
-        ],
-        ingredients: [
-            'minecraft:carrot', 'minecraft:carrot'
-        ]
-    })
-    event.recipes.botania.elven_trade('scrunt_test', ['botania:tiny_potato * 4 {display: {Name: \'{"text":"pahimar"}\'}}'], ['minecraft:potato', 'minecraft:potato'])
-
-    event.recipes.botania.mana_infusion({
-        id: 'coom',
-        output: 'minecraft:gold_block',
-        input: 'minecraft:brick',
-        mana: 500,
-        catalyst: 'minecraft:bricks'
-    })
-    event.recipes.botania.mana_infusion('coom2', 'minecraft:diamond_block', 'minecraft:brick', 1000, 'minecraft:gold_block', 'botania:flower_cycle')
-    event.recipes.botania.mana_infusion('tag_test','#minecraft:leaves', '#minecraft:coals', 1000, '#minecraft:logs')
-
-    event.recipes.botania.petal_apothecary({
-        id: 'vroom',
-        output: 'minecraft:clay_ball',
-        ingredients: ['minecraft:iron_ingot', 'minecraft:gold_ingot']
-    })
-    event.recipes.botania.petal_apothecary('gloom', 'minecraft:diamond', ['minecraft:clay_ball', 'minecraft:clay_ball'])
-
-    event.recipes.botania.pure_daisy({
-        id: 'woomy',
-        output: 'minecraft:coarse_dirt',
-        input: 'minecraft:coal_ore',
-        time: 20
-    })
-    event.recipes.botania.pure_daisy('yoom', 'minecraft:gold_ore', 'minecraft:bricks', 80)
-
-    event.recipes.botania.runic_altar({
-        id: 'roony',
-        output: 'minecraft:clay_ball',
-        ingredients: ['minecraft:feather', 'minecraft:feather'],
-        mana: 3000
-    })
-    event.recipes.botania.runic_altar('loomy', 'minecraft:feather', ['minecraft:clay_ball','minecraft:clay_ball'], 500)
-
-    event.recipes.botania.terra_plate({
-        id: 'all_the_mana_is_gone',
-        result: 'minecraft:clay_ball',
-        ingredients: ['minecraft:diamond','botania:tiny_potato'],
-        mana: 1500000
-    })
-    event.recipes.botania.terra_plate('awful', 'minecraft:golden_apple*10', ['minecraft:potato','minecraft:golden_carrot'], 1500)
-})
 }
